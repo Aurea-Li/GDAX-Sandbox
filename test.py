5ed4edc89003d2ed1d3dbe620363a8db89432337
Aurea-Li <fu.sakura@gmail.com>
2018-02-16 21:51:56 -0800

    minor text edit

--- test.py
+++ test.py
@@ -1,16 +1,34 @@
 import gdax
 import json
+from AUTH import auth
+
+# API key
+auth = auth()
 
 public = gdax.PublicClient()
 
-def get_products_USD():
-	'''Modify JSON object get_products to only store USD'''
-	return [products for products in public.get_products() if products['quote_currency'] == 'USD']
+def get_products(currency='USD'):
+	'''Modify JSON object get_products to get quotes in various currencies
+	Default behavior: USD
+	Options: BTC, USD, GBR, EUR
+	'''
+	return [products for products in public.get_products() if products['quote_currency'] == currency.upper()]
+
+def get_accountsa(i=0):
+	''' Default behavior: Return account inventory if balance is greater than 0
+	Options: -1 to return all, else i'''
+	return [item for item in auth.get_accounts() if float(item['balance']) > i]
 
 
 def printJSON(JSON):
-	''' Prints JSON object in readable format. '''
-	print json.dumps(JSON, indent = 4)
+	''' Prints JSON object in readable format'''
+	print(json.dumps(JSON, indent=4))
+
+# Buying .5 ethereum
+# print(auth.buy(type = 'market', size = '0.5', product_id = 'ETH-USD'))
+# print(auth.sell(price = '5000', size = '0.5', product_id = 'BTC-USD'))
+# printJSON(auth.get_orders())
+# printJSON(auth.get_accounts())
 
 
-printJSON(get_products_USD())
+
